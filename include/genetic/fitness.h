d4332bb25ad7f7ab49d85dad1a3073ecc4ea10a7
Paul T <developer.paul.123@gmail.com>
2023-09-14 14:02:56 -0400

    Re-architect fitness operations
    Updated fitness operations in line with proposed new architecture

--- include/genetic/fitness.h
+++ include/genetic/fitness.h
@@ -1,39 +1,18 @@
 #pragma once
+#include <concepts>
 #include <numeric>
 #include <ranges>
 
+#include "genetic/details/concepts.h"
+#include "genetic/op/fitness/accumulation.h"
+#include "genetic/op/fitness/composite_fitness.h"
+#include "genetic/op/fitness/element_wise_comparison.h"
+
 namespace dp::genetic {
-    struct accumulation_fitness {
-        template <std::ranges::range T, typename U = double>
-        U operator()(T&& value) {
-            return std::accumulate(std::ranges::begin(value), std::ranges::end(value), U{});
-        }
-    };
-
-    template <std::ranges::range T>
-    struct element_wise_comparison {
-        explicit element_wise_comparison(T solution) : solution_(std::move(solution)) {}
-        template <typename U = double>
-        U operator()(const T& value) {
-            U score{};
-            const auto sol_length = std::ranges::distance(solution_);
-            const auto val_length = std::ranges::distance(value);
-            std::ranges::range_difference_t<T> index;
-            for (index = 0; index < std::min(sol_length, val_length); ++index) {
-                auto solution_val = solution_.at(index);
-                auto val = value.at(index);
-                if (val == solution_val) score += 10;
-            }
-
-            // subtract for difference in length
-            if (sol_length != val_length) {
-                score -= std::abs(sol_length - val_length);
-            }
-
-            return score;
-        }
-
-      private:
-        T solution_;
-    };
+    template <std::ranges::range Range, typename FitnessOp,
+              typename T = std::invoke_result_t<std::decay_t<FitnessOp>, Range>>
+        requires concepts::fitness_operator<std::decay_t<FitnessOp>, Range>
+    constexpr T evaluate_fitness(const Range& range, FitnessOp&& fitness_op) {
+        return std::invoke(std::forward<FitnessOp>(fitness_op), range);
+    }
 }  // namespace dp::genetic
